1d7462ea2d43df4082f809b61a71215525df067f
Rahat Kabir Ifty Mac <iftyrahatkabir@gmail.com>
2025-07-02 00:30:13 +0600

    Delete at head

---
+++ Delete_at_head_3_.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+class Node
+{
+    public:
+        int val;
+        Node* next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = next;
+    }
+};
+
+void insert_at_tail(Node* &head, Node* &tail, int val)
+{
+    Node* newnode = new Node(val);
+
+    if(head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    tail->next = newnode;
+    tail = newnode; 
+}
+
+void print_linked_list(Node* head)
+{
+    Node* temp = head;
+    while(temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+}
+
+void delete_at_head(Node* &head)
+{
+    Node* deletenode = head;
+    head = head->next;
+
+    delete deletenode;
+}
+
+
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;  
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if(val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+
+    delete_at_head(head);
+
+    print_linked_list(head); 
+    
+
+    return 0;
+}
