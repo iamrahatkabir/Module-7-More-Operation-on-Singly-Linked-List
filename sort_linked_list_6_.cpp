b770c5aabee78624518333eeacd648de7f91b9eb
Rahat Kabir Ifty Mac <iftyrahatkabir@gmail.com>
2025-07-02 03:11:23 +0600

    Sort liked list

---
+++ sort_linked_list_6_.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+class Node
+{
+    public:
+        int val;
+        Node* next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = next;
+    }
+};
+
+void insert_at_tail(Node* &head, Node* &tail, int val)
+{
+    Node* newnode = new Node(val);
+
+    if(head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    tail->next = newnode;
+    tail = newnode; 
+}
+
+void print_linked_list(Node* head)
+{
+    Node* temp = head;
+    while(temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+}
+
+
+void sort_linked_list(Node* head)
+{
+    for(Node* i = head; i->next != NULL; i = i->next)
+    {
+        for(Node* j = i->next; j != NULL; j = j->next)
+        {
+            if(i->val > j->val)
+            {
+                swap(i->val, j->val);
+            }
+        }
+    }
+}
+
+int main()
+{
+    Node* head = NULL;
+    Node* tail = NULL;  
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if(val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+
+    sort_linked_list(head);
+    print_linked_list(head); 
+    
+
+    return 0;
+}
